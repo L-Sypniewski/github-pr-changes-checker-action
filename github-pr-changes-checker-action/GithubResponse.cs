b8bdafb19ba00a8d62e3d1ab8d52935e84c14b17
Łukasz Sypniewski <l.sypniewski@gmail.com>
2022-08-09 16:01:33 +0200

    Remove unused properties from response model

--- github-pr-changes-checker-action/GithubResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace GithubPrChangesChecker;
-
-public record GithubResponse(string sha, string filename, string status, int additions, int deletions, int changes, string blob_url, string raw_url,
-string contents_url, string patch);

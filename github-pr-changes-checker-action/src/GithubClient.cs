e17af3d0b3d57758181b5118114a0e2e737e618c
Łukasz Sypniewski <l.sypniewski@gmail.com>
2022-08-10 12:51:06 +0200

    GithubClient returns an array of strings

--- github-pr-changes-checker-action/src/GithubClient.cs
+++ github-pr-changes-checker-action/src/GithubClient.cs
@@ -12,13 +12,16 @@ public class GithubClient
         _httpClient = httpClient;
     }
 
-    public async Task<GithubFileChange[]> GetResponse(string owner, string name, string prNumber, string token)
+    public async Task<string[]> GetChangedProjectsNames(string owner, string name, string prNumber, string token)
     {
         AuthenticationHeaderValue authHeader = new("token", token);
         _httpClient.DefaultRequestHeaders.Authorization = authHeader;
 
         var requestUri = $"/repos/{owner}/{name}/pulls/{prNumber}/files";
-        var githubResponse = await _httpClient.GetFromJsonAsync<GithubFileChange[]>(requestUri);
-        return githubResponse ?? Array.Empty<GithubFileChange>();
+        var uriWithParams = $"{requestUri}?per_page=100&page=1";
+        var githubResponse = await _httpClient.GetFromJsonAsync<GithubFileChange[]>(uriWithParams);
+        
+        var projectNames = githubResponse?.Select(x => x.Filename.Split('/').First()).Distinct().ToArray();
+        return projectNames ?? Array.Empty<string>();
     }
 }
